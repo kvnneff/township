afc7c39a97ab1b9cd596b86e8501bc5702ad31bd
River Grimm <river.grimm@gmail.com>
2015-07-06 15:56:43 -0700

    update apps to use new app api

--- apps/discuss/posts/index.js
+++ apps/discuss/posts/index.js
@@ -1,17 +1,20 @@
-module.exports = function (db, options) {
+module.exports = function (options) {
   options = options || {}
-  var model = require('./model')(db, options)
-  var handler = require('./handler')(model, options)
-  var routes = require('./routes')(handler, options)
 
-  return {
-    name: 'posts',
-    model: model,
-    schema: model.schema,
-    handler: handler,
-    routes: routes,
-    serve: function (req, res) {
-      return routes.match(req, res)
+  return function (server) {
+    var model = require('./model')(server.db, options)
+    var handler = require('./handler')(model, options)
+    var routes = require('./routes')(handler, options)
+
+    return {
+      name: 'posts',
+      model: model,
+      schema: model.schema,
+      handler: handler,
+      routes: routes,
+      serve: function (req, res) {
+        return routes.match(req, res)
+      }
     }
   }
 }
