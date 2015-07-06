01fffe70bdeed787aba7a8696f46e4d32b4ce16f
River Grimm <river.grimm@gmail.com>
2015-07-06 14:49:02 -0700

    update schema to return a function

--- apps/schema/index.js
+++ apps/schema/index.js
@@ -1,14 +1,17 @@
-module.exports = function (server, options) {
+module.exports = function (options) {
   options = options || {}
-  var handler = require('./handler')(server, options)
-  var routes = require('./routes')(handler, options)
 
-  return {
-    name: 'schema',
-    handler: handler,
-    routes: routes,
-    serve: function (req, res) {
-      return routes.match(req, res)
+  return function (server) {
+    var handler = require('./handler')(server, options)
+    var routes = require('./routes')(handler, options)
+
+    return {
+      name: 'schema',
+      handler: handler,
+      routes: routes,
+      serve: function (req, res) {
+        return routes.match(req, res)
+      }
     }
   }
 }
